4648f177ea5edb3b3dd2e4a7f726aee891e39b96
Ben Pilgrim <ben@pilgrim.me.uk>
2020-07-19 14:31:44 +0100

    Fix line wrapping json issue in ActivityPub example 4

--- src/test/java/social/pantheon/activitystreams4j/W3CTests.java
+++ src/test/java/social/pantheon/activitystreams4j/W3CTests.java
@@ -61,7 +61,12 @@ public class W3CTests extends AbstractSerdesTest{
 
     @TestFactory
     Stream<DynamicTest> W3CActivityPubExamples() throws IOException {
-        return testExamplesFromW3CTechnicalReport("https://www.w3.org/TR/activitypub/");
+        Map<String, String> fixes = new HashMap<>();
+
+        // New line wrapping causes issues.
+        fixes.put("Example 4", "{\"@context\":\"https://www.w3.org/ns/activitystreams\",\"type\":\"Create\",\"id\":\"https://chatty.example/ben/p/51086\",\"to\":[\"https://social.example/alyssa/\"],\"actor\":\"https://chatty.example/ben/\",\"object\":{\"type\":\"Note\",\"id\":\"https://chatty.example/ben/p/51085\",\"attributedTo\":\"https://chatty.example/ben/\",\"to\":[\"https://social.example/alyssa/\"],\"inReplyTo\":\"https://social.example/alyssa/posts/49e2d03d-b53a-4c4c-a95c-94a6abf45a19\",\"content\":\"<p>Argh, yeah, sorry, I'll get it back to you tomorrow.</p> <p> I was reviewing the section on register machines, since it 's been a while since I wrote one.</p>\"}}");
+
+        return testExamplesFromW3CTechnicalReport("https://www.w3.org/TR/activitypub/", fixes);
     }
 
     private Stream<DynamicTest> testExamplesFromW3CTechnicalReport(String baseUrl) throws IOException {
